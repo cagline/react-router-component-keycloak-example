06344c164968d6db3eb417ca28805fe80812e665
Chanuka Asanka <cagline@gmail.com>
2020-05-22 12:38:22 +0530

    style change

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import { KeycloakProvider } from '@react-keycloak/web';
-import keycloak from './keycloak';
-
 import React from 'react';
-import './App.css';
+import { useKeycloak } from '@react-keycloak/web';
+import AuthrizedElement from '../components/AuthrizedElement';
 
-import { AppRouter } from './routes';
+const HomePage = () => {
+  const [keycloak, initialized] = useKeycloak();
 
-
-function App() {
   return (
-    <KeycloakProvider keycloak={keycloak}>
-      <div className="App">
-        <AppRouter />
-      </div>
-    </KeycloakProvider>
-  );
-}
+    <div>
+      <h1>Home Page</h1>
 
-export default App;
+      <strong>Anyone can access this page</strong>
+
+      <div className="card">
+        <h2>`RealmAdmin` user role can access below button</h2>
+        <small>button gose here</small>
+        <AuthrizedElement roles={['RealmAdmin']}><button>Realm Admin Acction Button</button></AuthrizedElement>
+
+        <hr />
+
+        <h2>`ClientAdmin` user role can access below button</h2>
+        <small>button gose here</small>
+        <AuthrizedElement roles={['ClientAdmin']}><button>Client Admin Acction Button</button></AuthrizedElement>
+      </div>
+
+      {initialized ?
+        keycloak.authenticated && <pre className="json">{JSON.stringify(keycloak, undefined, 2)}</pre>
+        : <h2>keycloak initializing ....!!!!</h2>
+      }
+    </div>
+  )
+}
+export default HomePage
